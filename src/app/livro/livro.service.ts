2ad7ff1945199ab53b0fd8277d9f3892e0ea11e0
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2022-05-24 21:02:10 -0300

    Implementação de Subject, Observable e Subscription

--- src/app/livro/livro.service.ts
+++ src/app/livro/livro.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from "@angular/core";
-import { Livro } from "./livro.model"
+import { Livro } from "./livro.model";
+import { Subject } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 
 export class LivroService{
   private livros:Livro[];
+  private listaLivrosAtualizada = new Subject<Livro[]>();
 
   getLivros(): Livro[]{
     return [...this.livros];
@@ -15,6 +17,11 @@ export class LivroService{
       id, titulo, autor, paginas
     };
     this.livros.push(livro);
+    this.listaLivrosAtualizada.next([...this.livros]);
     }
+
+  getListaLivrosAtualizadaObservable() {
+    return this.listaLivrosAtualizada.asObservable();
+  }
 
 }
--- src/app/livro/livros-lista/livros-lista.component.ts
+++ src/app/livro/livros-lista/livros-lista.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {Livro} from '../livro.model';
 import { LivroService } from '../livro.service';
+import { Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-livros-lista',
   templateUrl: './livros-lista.component.html',
   styleUrls: ['./livros-lista.component.css']
 })
-export class LivrosListaComponent implements OnInit {
+export class LivrosListaComponent implements OnInit, OnDestroy {
 
   livros: Livro[];
+  private livrosSubscription: Subscription;
 
   constructor(public livroService: LivroService) { }
 
@@ -36,5 +38,15 @@ export class LivrosListaComponent implements OnInit {
 
   ngOnInit(): void {
     this.livros = this.livroService.getLivros();
+    this.livrosSubscription = this.livroService
+    .getListaLivrosAtualizadaObservable()
+    .subscribe((livros: Livro[]) => {
+      this.livros = livros;
+    })
   }
+
+  ngOnDestroy(): void {
+      this.livrosSubscription.unsubscribe();
+  }
+
 }
