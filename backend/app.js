27472fcad4e5f1c3e06b08c6727cb0a07a5e8f70
Arthur <arthur.nascimento2020@outlook.com>
2022-06-05 11:41:30 -0300

    Configurando Endpoint
    Configurando a rota para extrair um objeto json salvo na api

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Chegou uma requisição');
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log('Chegou uma requisição');
+//     next();
+// });
 
-app.use((req, res, next) => {
-    res.send('Hello from the backend');
+const livros = [
+    {   
+        id: 1,
+        titulo: 'O Senhor dos Anéis',
+        autor: 'J.R.R. Tolkien',
+        paginas: '456'
+    },
+    {
+        id: 2,
+        titulo: 'O Hobbit',
+        autor: 'J.R.R. Tolkien',
+        paginas: '456'
+    }
+]
+    
+
+app.use('/api/livros',(req, res, next) => {
+    res.status(200).json({mensagem: "Tudo OK", objLivros: livros});
 });
 
 module.exports = app;
